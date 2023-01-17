82a62c0b62bcecc73823cc77623cad97bea8e79b
geomalv02 <87823141+geomalv02@users.noreply.github.com>
2023-01-17 14:27:00 -0600

    Add files via upload

---
+++ intro3.js
@@ -0,0 +1,15 @@
+var data =[
+    {nombre: 'nohemy', edad: '19'},
+]
+undefined
+var data =[
+    {nombre: 'nohemy', edad: '19'},
+    {nombre: 'marilyn', edad: '53'},
+    {nombre: 'pirlo', edad: '40'}
+]
+undefined
+var data =[
+    {nombre: 'nohemy', edad: '19'},
+    {nombre: 'marilyn', edad: '53'},
+    {nombre: 'pirlo', edad: '40'},
+]
---
+++ intro4.js
@@ -0,0 +1,12 @@
+var alumno = {
+    nombre: 'Nohemy',
+    apellidos: 'Zelaya',
+
+
+    mostrarDatos: function(){
+        console.log(`${this.nombre},
+${this.apellido}`)
+    }
+}
+alumno
+alumno.mostrarDatos()
